1a3423de1e208cbbac11f98d68bc92f0e03ea888
Montazer Malik <mntthr1122@gmail.com>
2022-05-11 15:03:57 +0300

    Fix design errors
    Fix some design errors

--- resources/views/livewire/form/add.blade.php
+++ resources/views/livewire/form/add.blade.php
@@ -123,49 +123,45 @@
 
             <!--  Family wife name -->
             <div x-show="formStep === 5" class="space-y-4">
-                <div class="px-10">
-                    <div class="grid grid-cols-10 gap-5 mt-10">
-                        <div class="col-span-10 w-auto">
-                            <x-input label="اسم ربة الاسرة" placeholder="ادخل الاسم" />
-                        </div>
+                <div class="grid grid-cols-10 gap-5 px-10">
+                    <div class="col-span-10 w-auto">
+                        <x-input label="اسم ربة الاسرة" placeholder="ادخل الاسم" />
                     </div>
-                    <div class="grid grid-cols-12 gap-10 mt-10">
-                        <div>
-                            <h1 class="text-lg">النسب</h1>
-                        </div>
-                        <div class="col-span-4 w-auto">
-                            <div class="grid grid-cols-4 gap-6">
-                                <div>
-                                    <label>سيد</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
-                                <div>
-                                    <label>عامي</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
+                    <div>
+                        <h1 class="text-lg">النسب</h1>
+                    </div>
+                    <div class="col-span-4 w-auto">
+                        <div class="grid grid-cols-4 gap-6">
+                            <div>
+                                <label>سيد</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
+                            </div>
+                            <div>
+                                <label>عامي</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
                             </div>
                         </div>
-                        <div>
-                            <h1 class="text-lg">الحالة</h1>
-                        </div>
-                        <div class="col-span-6 w-auto">
-                            <div class="grid grid-cols-4 gap-6">
-                                <div>
-                                    <label>حي</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
-                                <div>
-                                    <label>متوفي</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
-                                <div>
-                                    <label>مطلقة</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
-                                <div>
-                                    <label>أرملة</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
+                    </div>
+                    <div>
+                        <h1 class="text-lg">الحالة</h1>
+                    </div>
+                    <div class="col-span-4 w-auto">
+                        <div class="grid grid-cols-4 gap-6">
+                            <div>
+                                <label>حي</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
+                            </div>
+                            <div>
+                                <label>متوفي</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
+                            </div>
+                            <div>
+                                <label>مطلقة</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
+                            </div>
+                            <div>
+                                <label>أرملة</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
                             </div>
                         </div>
                     </div>
@@ -192,36 +188,32 @@
 
             <!--  Living -->
             <div x-show="formStep === 7" class="space-y-4">
-                <div class="px-10">
-                    <div class="grid grid-cols-10 gap-10">
-                        <div>
-                            <h1 class="text-lg">نوع السكن</h1>
-                        </div>
-                        <div class="col-span-4 w-auto">
-                            <div class="grid grid-cols-4 gap-6">
-                                <div>
-                                    <label>ملك</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
-                                <div>
-                                    <label>تجاوز</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
-                                <div>
-                                    <label>ايجار</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
-                                <div>
-                                    <label>زراعي</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
+                <div class="grid grid-cols-10 gap-10 px-10">
+                    <div>
+                        <h1 class="text-lg">نوع السكن</h1>
+                    </div>
+                    <div class="col-span-4 w-auto">
+                        <div class="grid grid-cols-4 gap-6">
+                            <div>
+                                <label>ملك</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
+                            </div>
+                            <div>
+                                <label>تجاوز</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
+                            </div>
+                            <div>
+                                <label>ايجار</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
+                            </div>
+                            <div>
+                                <label>زراعي</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
                             </div>
                         </div>
                     </div>
-                    <div class="grid grid-cols-10 gap-5 mt-14">
-                        <div class="col-span-10 w-auto">
-                            <x-input label="مقدار الايجار الشهري" placeholder="ادخل المقدار" />
-                        </div>
+                    <div class="col-span-5 w-auto">
+                        <x-input label="مقدار الايجار الشهري" placeholder="ادخل المقدار" />
                     </div>
                 </div>
                 @livewire('form.accessories.buttons')
@@ -246,40 +238,36 @@
 
             <!--  Have Salary & Amount -->
             <div x-show="formStep === 9" class="space-y-4">
-                <div class="px-10">
-                    <div class="grid grid-cols-10 gap-10">
-                        <div>
-                            <h1 class="text-lg">هل يتقاضى راتب ؟</h1>
-                        </div>
-                        <div class="col-span-4 w-auto">
-                            <div class="grid grid-cols-5 gap-6">
-                                <div>
-                                    <label>تقاعد</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
-                                <div>
-                                    <label>رعاية</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
-                                <div>
-                                    <label>مؤسسة</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
-                                <div>
-                                    <label>مساعدات</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
-                                <div>
-                                    <label>حكومي</label>
-                                    <x-radio id="top-label" lg wire:model.defer="model" />
-                                </div>
+                <div class="grid grid-cols-10 gap-10 px-10">
+                    <div class="col-span-2">
+                        <h1 class="text-lg">هل يتقاضى راتب ؟</h1>
+                    </div>
+                    <div class="col-span-8 w-auto">
+                        <div class="grid grid-cols-5 gap-6">
+                            <div>
+                                <label>تقاعد</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
+                            </div>
+                            <div>
+                                <label>رعاية</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
+                            </div>
+                            <div>
+                                <label>مؤسسة</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
+                            </div>
+                            <div>
+                                <label>مساعدات</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
+                            </div>
+                            <div>
+                                <label>حكومي</label>
+                                <x-radio id="top-label" lg wire:model.defer="model" />
                             </div>
                         </div>
                     </div>
-                    <div class="grid grid-cols-10 gap-5 mt-14">
-                        <div class="col-span-10 w-auto">
-                            <x-input label="مقداره" placeholder="ادخل المقدار" />
-                        </div>
+                    <div class="col-span-10 w-auto">
+                        <x-input label="مقداره" placeholder="ادخل المقدار" />
                     </div>
                 </div>
                 @livewire('form.accessories.buttons')
@@ -295,7 +283,7 @@
                     </div>
                     <div>
                         <div>
-                            <x-input label="رقم هاتف الاب" placeholder="ادخل الرقم " />
+                            <x-input label="رقم هاتف الام" placeholder="ادخل الرقم " />
                         </div>
                     </div>
                 </div>
