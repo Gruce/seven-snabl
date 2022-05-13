965dc1e637fd3ca6d8068034f03ff8cfad375749
hassan_000 <salem.cf11@gmail.com>
2022-05-13 16:31:16 +0300

    z

---
+++ app/Http/Livewire/City/Show.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\City;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return view('livewire.city.show');
+    }
+}
--- resources/views/livewire/city/add.blade.php
+++ resources/views/livewire/city/add.blade.php
@@ -1,3 +1,14 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <div class="grid grid-cols-2 gap-5 mt-5">
+        <div>
+            <x-input wire:model.defer="" label=" اسم المنطقة" placeholder="ادخل اسم المنطقة " />
+        </div>
+        <div>
+            <x-input wire:model.defer="" label=" رمز المنطقة " placeholder="ادخل الرمز " />
+        </div>
+    </div>
+
+    <x-button class="mt-10 w-full" wire:click="form.add()" primary label="اضافة" />
+
+    
 </div>
