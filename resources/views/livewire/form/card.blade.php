60025c654d5f9d8c49be88f9738d9b0151fbafd3
Montazer Malik <64214350+ste7een@users.noreply.github.com>
2022-05-13 17:38:54 +0300

    Update card.blade.php

--- resources/views/livewire/form/card.blade.php
+++ resources/views/livewire/form/card.blade.php
@@ -2,30 +2,22 @@
     @for ($i=0 ; $i < 10 ; $i++)
         <x-card shadow=false title="التنومة">
             <x-slot name="action">
-                <button class="rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-600">
-                    <x-icon name="dots-vertical" class="w-4 h-4 text-gray-500" />
-                </button>
+                <span class="text-lg">12/2/2005</span>
             </x-slot>
             <x-slot name="footer">
                 <div class="flex justify-between items-center">
                     <x-button negative icon="x" />
-                    <x-button primary icon="view-grid-add" label="gh" href="{{route('show')}}" />
+                    <x-button primary icon="view-grid-add" href="{{route('show')}}" />
                 </div>
             </x-slot>
             <!-- Card Content -->
             <div class="flex justify-center">
-                <div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
+                <div class="grid lg:grid-cols-2 sm:grid-cols-1 gap-4">
                     <div>
                         <span class="text-lg">جبار علي محمد</span>
                     </div>
                     <div>
-                        <span class="text-lg">12/2/2005</span>
-                    </div>
-                    <div>
-                        <span class="text-lg">مستوى الفقر : B2</span>
-                    </div>
-                    <div>
-                        <span class="text-lg">عدد افراد الاسرة</span>
+                        <span class="text-lg">عدد الافراد : 43</span>
                     </div>
                 </div>
             </div>
