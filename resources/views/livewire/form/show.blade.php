8637ca2fa6ed567483f2b0e3cecf046d304ee1f4
hassan_000 <salem.cf11@gmail.com>
2022-05-13 18:55:48 +0300

    show

--- resources/views/livewire/form/show.blade.php
+++ resources/views/livewire/form/show.blade.php
@@ -41,7 +41,279 @@
             </h1>
         </div>
         <x-card shadow=false class=" border-transparent">
-            <h1>df</h1>
+            <div class="relative overflow-x-auto  sm:rounded-lg mt-10" dir="rtl">
+                <table class="w-full text-sm text-right text-gray-500 ">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                               مستوى الفقر
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                اسم رب الاسرة
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                 النسب
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                الحالة
+                            </th>
+
+                            <th scope="col" class="px-6 py-3">
+                                عمل رب الاسرة
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                  مقدار الدخل الشهري
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                الاجراءات
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b ">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900  whitespace-nowrap">
+                                B
+                            </th>
+                            <td class="px-6 py-4">
+                                حسن حازم جداوي
+                            </td>
+                            <td class="px-6 py-4">
+                                سيد
+                            </td>
+                            <td class="px-6 py-4">
+                                متوفي
+                            </td>
+                            <td class="px-6 py-4">
+                                موظف
+                            </td>
+                            <td class="px-6 py-4">
+                            298
+                            </td>
+                            <td class="px-6 py-4 text-right">
+                                <x-button class="text-gray-500" icon="pencil-alt" secondary   />
+                                <x-button class="text-red" icon="trash" negative  />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="relative overflow-x-auto  sm:rounded-lg mt-10" dir="rtl">
+                <table class="w-full text-sm text-right text-gray-500 ">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                اسم الزوجة
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                النسب
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                الحالة
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                رقم هاتف الاب
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                رقم هاتف الام
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                الاجراءات
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b ">
+
+                            <td class="px-6 py-4">
+                                هدى ناصر حسين
+                            </td>
+                            <td class="px-6 py-4">
+                                سيد
+                            </td>
+                            <td class="px-6 py-4">
+                                متوفية
+                            </td>
+                            <td class="px-6 py-4">
+                                07729454553
+                            </td>
+                            <td class="px-6 py-4">
+                                07719458827
+                            </td>
+                            <td class="px-6 py-4 text-right">
+                                <x-button class="text-gray-500" icon="pencil-alt" secondary   />
+                                <x-button class="text-red" icon="trash" negative  />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="relative overflow-x-auto  sm:rounded-lg mt-10" dir="rtl">
+                <table class="w-full text-sm text-right text-gray-500 ">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                عنوان السكن
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                اقرب نقطة دالة
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                نوع السكن
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                مقدار الايجاد الشهري
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                الاجراءات
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b  ">
+                            <td class="px-6 py-4">
+                                التنومة
+                            </td>
+                            <td class="px-6 py-4">
+                                شارع 34
+                            </td>
+                            <td class="px-6 py-4">
+                                ملك
+                            </td>
+                            <td class="px-6 py-4">
+                                0
+                            </td>
+                            <td class="px-6 py-4 text-right">
+                                <x-button class="text-gray-500" icon="pencil-alt" secondary   />
+                                <x-button class="text-red" icon="trash" negative  />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="relative overflow-x-auto  sm:rounded-lg mt-10" dir="rtl">
+                <table class="w-full text-sm text-right text-gray-500 ">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                عمل افراد الاسرة
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                  عددهم
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                 هل يتقاضى راتب
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                  مقداره
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                الاجراءات
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b  ">
+                            <td class="px-6 py-4">
+                            لايوجد
+                            </td>
+                            <td class="px-6 py-4">
+                                4
+                            </td>
+                            <td class="px-6 py-4">
+                                نعم
+                            </td>
+                            <td class="px-6 py-4">
+                                346
+                            </td>
+                            <td class="px-6 py-4 text-right">
+                                <x-button class="text-gray-500" icon="pencil-alt" secondary   />
+                                <x-button class="text-red" icon="trash" negative  />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="mt-10 text-center font-semibold">
+                <h1>
+                    اسماء افراد العائلة
+                </h1>
+            </div>
+            <div class="relative overflow-x-auto  sm:rounded-lg mt-10" dir="rtl">
+                <table class="w-full text-sm text-right text-gray-500 ">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                  #
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                  الاسم
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                   الصلة
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                  التولد
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                النسب
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                المهنة
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                الحالة الصحية
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                 الاجراءات
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b  ">
+                            <td class="px-6 py-4">
+                            1
+                            </td>
+                            <td class="px-6 py-4">
+                                حسن كاظم حميد
+                            </td>
+                            <td class="px-6 py-4">
+                                ابن
+                            </td>
+                            <td class="px-6 py-4">
+                                ابو الخصيب
+                            </td>
+                            <td class="px-6 py-4">
+                                سيد
+                            </td>
+                            <td class="px-6 py-4">
+                                عاطل
+                            </td>
+                            <td class="px-6 py-4">
+                                مريض
+                            </td>
+                            <td class="px-6 py-4 text-right">
+                                <x-button class="text-gray-500" icon="pencil-alt" secondary   />
+                                <x-button class="text-red" icon="trash" negative  />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <textarea class="w-full" name="" id="" cols="10" rows="3" placeholder="الملاحظات"></textarea>
+            </div>
+            <div class="flex flex-row justify-between mt-5">
+                <div>
+                    <h1>
+                        -:اسم المتابع وتوقيعه
+                    </h1>
+                </div>
+                <div>
+                    <h1>
+                        تاريخ الكشف 2022/5/13
+                    </h1>
+                </div>
+            </div>
         </x-card>
     </x-card>
 </div>
