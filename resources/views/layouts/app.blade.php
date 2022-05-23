2bb25f52ff9c81ce27e069941d3e25c43d8743eb
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-05-23 21:00:29 +0300

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,17 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
         <link rel="icon" href="{{ asset('img/a.png') }}" type="image/x-icon">
-        {{-- ` Fonts  --}}
-
+        {{--  Fonts  --}}
         <link href="https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@400&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.6/dist/flowbite.min.css" />
-
         @livewireStyles
         @wireUiScripts
         @laravelPWA
