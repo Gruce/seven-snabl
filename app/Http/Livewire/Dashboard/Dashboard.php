35d68aa555a4b92158c7853c30edd4970faaeee6
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-05-23 03:53:48 +0300

    ad

--- app/Http/Livewire/Dashboard/Dashboard.php
+++ app/Http/Livewire/Dashboard/Dashboard.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Http\Livewire\Dashboard;
+
+use App\Models\City;
 use App\Models\Form;
+use App\Models\User;
 use Livewire\Component;
 
 class Dashboard extends Component
@@ -11,6 +14,9 @@ class Dashboard extends Component
         $this->total_form = Form::count() ?? 0;
         $this->total_form_month = Form::whereMonth('created_at', date('m'))->count() ?? 0;
         $this->total_form_year = Form::whereYear('created_at', date('Y'))->count() ?? 0;
+
+        $this->users = User::get();
+        $this->ctyies = City::get();
     }
     public function render()
     {
--- resources/views/livewire/dashboard/dashboard.blade.php
+++ resources/views/livewire/dashboard/dashboard.blade.php
@@ -1,31 +1,91 @@
 <div>
-    <x-card class="border-1">
+    <div class="border-3 bg-slate-200 rounded-2xl py-4 px-4">
         <h1 class="text-2xl mb-4 mr-2 font-semibold">احصائيات عامة</h1>
         <div class="flex flex-row ">
-            <div class="basis-1/3 ml-3">
-                <x-card class="rounded-full">
+            <div class="basis-1/3 ml-3 bg-gray-100  rounded-2xl py-4 px-4">
+                <div class="rounded-full">
                     <div class="flex flex-col py-1 text-center ">
                         <span class="text-black-500 font-semibold text-lg">الاستمارات</span>
                         <span class="text-black-500">{{$total_form}}</span>
                     </div>
-                </x-card>
+                </div>
             </div>
-            <div class="basis-1/3 ml-3">
-                <x-card>
+            <div class="basis-1/3 ml-3 bg-gray-100  rounded-2xl py-4 px-4">
+                <div>
                     <div class="flex flex-col py-1 text-center">
                         <span class="text-black-400 text-lg font-semibold">عدد الاستمارات<small>(لهذا الشهر)</small></span>
                         <span class="text-black-500">{{$total_form_month}}</span>
                     </div>
-                </x-card>
+                </div>
             </div>
-            <div class="basis-1/3">
-                <x-card>
+            <div class="basis-1/3 bg-gray-100  rounded-2xl py-4 px-4">
+                <div>
                     <div class="flex flex-col py-1 text-center">
                         <span class="text-black-400 text-lg font-semibold">عدد الاستمارات<small>(لهذه السنة)</small></span>
                         <span class="text-black-500">{{$total_form_year}}</span>
                     </div>
-                </x-card>
+                </div>
             </div>
         </div>
-    </x-card>
+    </div>
+    <br>
+    <div>
+        <div class="border-3 bg-slate-200 rounded-2xl py-4 px-4">
+            <div class="flex justify-between">
+                <div>
+                    <h1 class="text-2xl mb-4 mr-2 font-semibold">احصائيات المناطق</h1>
+                    <div class="flex flex-row ">
+                        <div class="basis-1/3 ml-3 bg-gray-100  rounded-2xl py-4 px-4">
+                            <div class="rounded-full">
+                                <div class="flex flex-col py-1 text-center ">
+                                    <span class="text-black-500 font-semibold text-lg">الاستمارات</span>
+                                    <span class="text-black-500">{{$total_form}}</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div>
+                    <h1 class="text-2xl mb-4 mr-2 font-semibold">احصائيات المخولون</h1>
+                    <div class="flex flex-row ">
+                        <div class="w-full ml-3 bg-gray-100  rounded-2xl py-4 px-4">
+                            <div class=" w-full">
+                                <table class="table w-full">
+                                    <!-- head -->
+                                    <thead>
+                                        <tr>
+                                            <th>الاسم</th>
+                                            <th>الاستمارات</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        
+                                        <tr>
+                                            <th>
+                                                <div class="flex items-center space-x-3">
+                                                    <div>
+                                                        <a href="#" class="font-bold btn-link">
+                                                            اجمالى
+                                                        </a>
+                                                        <div class="text-sm opacity-50"> -</div>
+                                                    </div>
+                                                </div>
+                                            </th>
+                                            <td class="py-4 px-6 text-sm font-medium text-gray-900">
+                                                wer
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </div>
