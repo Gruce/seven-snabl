2d06b312d451a1a95e7e3862249e9f34fc9904c5
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-05-24 10:01:55 +0300

    Update add.blade.php

--- resources/views/livewire/city/add.blade.php
+++ resources/views/livewire/city/add.blade.php
@@ -1,14 +1,17 @@
 <div>
     <div class="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-3 mt-5">
         <div>
-            <x-input wire:model.defer="city.name" label=" اسم المنطقة" placeholder="ادخل اسم المنطقة " />
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">اسم المنطقة</label>
+            <input wire:model.defer="city.code" type="text"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ادخل اسم المنطقة" >
+            @error('city.name') <span class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</span> @enderror
+
         </div>
         <div>
-            <x-input wire:model.defer="city.code" label=" رمز المنطقة " placeholder="ادخل الرمز " />
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"> رمز المنطقة</label>
+            <input wire:model.defer="city.code" type="text"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ادخل الرمز " >
+            @error('city.code') <span class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</span> @enderror
         </div>
     </div>
-
-    <x-button class="mt-10" x-on:click="close" wire:click="save" primary label="اضافة" />
-
+        <button  wire:click="save" type="button" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">اضافة</button>
 
 </div>
