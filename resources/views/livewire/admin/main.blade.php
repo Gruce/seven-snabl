9bd2a9d81267335318b749b263b602635f5ae38a
hassan_000 <salem.cf11@gmail.com>
2022-05-25 16:38:29 +0300

    ن

--- resources/views/livewire/admin/main.blade.php
+++ resources/views/livewire/admin/main.blade.php
@@ -29,10 +29,7 @@
                     <div class="p-6 space-y-6">
                         @livewire('admin.add')
                     </div>
-                    <!-- Modal footer -->
-                    <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-                        <button data-modal-toggle="user-modal"  type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">اغلاق</button>
-                    </div>
+                   
                 </div>
             </div>
         </div>
