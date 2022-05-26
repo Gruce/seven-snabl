2a3bcf50a9498f713d2b39895f81ddff64727af7
Hassan Alkhalidi <gruceing@gmail.com>
2022-05-27 01:53:38 +0300

    asd

--- resources/views/livewire/form/main.blade.php
+++ resources/views/livewire/form/main.blade.php
@@ -1,7 +1,7 @@
 @section('title', 'الكشوفات')
 <div>
-    <div class="mt-3" x-data="{ open: false, filter: false }">
-        <div class="grid p-2 mb-3 rounded-lg lg:grid-cols-2 sm:grid-cols-1 gap-2 bg-slate-50">
+    <div class="mt-3" x-data="{ open: false, filter: false, addGiven: false }">
+        <div class="grid gap-2 p-2 mb-3 rounded-lg lg:grid-cols-2 sm:grid-cols-1 bg-slate-50">
             {{-- ADD --}}
 
             <div class="flex justify-start">
@@ -14,14 +14,14 @@
             <div class="flex justify-end">
                 <input placeholder="البحث عن رقم الاستمارة" wire:model="filter.search" class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-30 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     @keydown.enter.prevent="$refresh" />
-                <button  @click="filter = ! filter" class=" text-gray-200 bg-blue-700 rounded-lg w-16  mr-4"><i
+                <button  @click="filter = ! filter" class="w-16 mr-4 text-gray-200 bg-blue-700 rounded-lg "><i
                         class="fa-solid fa-filter"></i></button>
             </div>
         </div>
         {{-- FILTERS --}}
         <div x-show="filter" class="grid lg:grid-cols-3 sm:grid-cols-1 ">
             <select wire:model="filter.city_id"
-                class=" bg-white border   border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 mr-2  ">
+                class="mr-2 text-sm text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500">
                     <option value="" >اختر المنطقة</option>
                 @forelse ($cities as $city)
                     <option value="{{$city->id}}">{{$city->name}}</option>
@@ -30,7 +30,7 @@
                 @endforelse
             </select>
             <select  wire:model="filter.person.level"
-                class=" bg-white border   border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 mr-2  ">
+                class="mr-2 text-sm text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500">
                 <option value="" >مستوى الفقر</option>
                 <option value="1">B1</option>
                 <option value="2">B2</option>
@@ -38,7 +38,7 @@
                 <option value="4">B4</option>
             </select>
             <select  wire:model="filter.review"
-                class=" bg-white border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 mr-2  ">
+                class="mr-2 text-sm text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500">
                 <option value="" >الحالة</option>
                 <option value="1" >تمت المراجعة</option>
                 <option value="2">لم تتم المراجعة</option>
@@ -47,13 +47,13 @@
         </div>
 
         {{-- ADD MODAL --}}
-        <div id="form-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden absolute top-0 right-0 left-0 z-50 w-full md:inset-0  md:h-full">
-            <div class="relative p-4 w-full max-w-4xl  md:h-auto sm:h-auto">
+        <div id="form-modal" tabindex="-1" class="absolute top-0 left-0 right-0 z-50 hidden w-full overflow-x-hidden overflow-y-auto md:inset-0 md:h-full">
+            <div class="relative w-full max-w-4xl p-4 md:h-auto sm:h-auto">
                 <!-- Modal content -->
                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                     <!-- Modal header -->
 
-                    <div class="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
+                    <div class="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
                         <div>
                             <h3 class="text-xl font-medium text-gray-900 dark:text-white">
                                 اضافة استمارة
@@ -76,14 +76,15 @@
             </div>
         </div>
 
+        <div x-show="addGiven">
+            <livewire:give.add />
+        </div>
+
 
         <div class="grid gap-2 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
             {{-- {{dg($forms['0']->head_family->is_mr_name)}} --}}
             @forelse ($forms as $form)
-                <div class="mb-1 mt-5">
-                    <livewire:form.card :form="$form" key="{{now()}}" />
-                    {{-- @livewire('form.card', ['form' => $form] , key(now())) --}}
-                </div>
+                <livewire:form.card :form="$form" key="form-{{now()}}-{{$form->id}}" />
             @empty
                 <div class="col-span-3 mt-4 ">
                     <h1 class="text-center">لايوجد</h1>
@@ -93,6 +94,4 @@
 
         {{-- {{ $forms->links() }} --}}
     </div>
-
-    @livewire('give.add', ['form' => $form_id ?? 1])
 </div>
