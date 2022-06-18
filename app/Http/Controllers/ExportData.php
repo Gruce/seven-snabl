3776f7d65afeca024cea191a8c070fb28e1592d3
hassan_000 <salem.cf11@gmail.com>
2022-06-18 17:01:22 +0300

    ZX

--- app/Http/Controllers/ExportData.php
+++ app/Http/Controllers/ExportData.php
@@ -4,26 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Form;
+
 class ExportData extends Controller
 {
 
 public function export()
 {
-     $posts = DB::table('forms')->get()->toArray();
-     
-     $S=DB::table('family_members')->get()->toArray();
-     $f=array_merge($posts,$S);
+    $data = DB::statement("SET names 'utf8'");
+    $data=[];
+     $forms =Form::all();
+        foreach ($forms as $form) {
+            $data[] = [
+                'id' => $form->id,
+                'name' => $form->family_members()->first()->name,
+                'email' => $form->email,
+                'phone' => $form->phone,
+                'address' => $form->address,
+                'city' => $form->city,
+                'state' => $form->state,
+                'zip' => $form->zip,
+                'country' => $form->country,
+                'level' => $form->level,
+            ];}
 
-    $data = array(
-        array("firstname" => "Mary", "lastname" => "Johnson", "age" => 25 ,"family_members"=>"John"),
-        array("firstname" => "Amanda", "lastname" => "Miller", "age" => 18,"family_members"=>"Mary"),
-        array("firstname" => "James", "lastname" => "Brown", "age" => 31,"family_members"=>"Amanda"),
-        array("firstname" => "Patricia", "lastname" => "Williams", "age" => 7,"family_members"=>"James"),
-        array("firstname" => "Michael", "lastname" => "Davis", "age" => 43,"family_members"=>"Patricia"),
-        array("firstname" => "Sarah", "lastname" => "Miller", "age" => 24,"family_members"=>"Michael"),
-        array("firstname" => "Patrick", "lastname" => "Miller", "age" => 27,"family_members"=>"Sarah"),
-      );
-      dd($posts,$data);
+    //  $S=DB::table('family_members')->get()->toArray();
+    //   $f=array_merge($posts);
+
+    // $data = array(
+    //     array("firstname" => "Mary", "lastname" => "Johnson", "age" => 25 ,"family_members"=>"John"),
+    //     array("firstname" => "Amanda", "lastname" => "Miller", "age" => 18,"family_members"=>"Mary"),
+    //     array("firstname" => "James", "lastname" => "Brown", "age" => 31,"family_members"=>"Amanda"),
+    //     array("firstname" => "Patricia", "lastname" => "Williams", "age" => 7,"family_members"=>"James"),
+    //     array("firstname" => "Michael", "lastname" => "Davis", "age" => 43,"family_members"=>"Patricia"),
+    //     array("firstname" => "Sarah", "lastname" => "Miller", "age" => 24,"family_members"=>"Michael"),
+    //     array("firstname" => "Patrick", "lastname" => "Miller", "age" => 27,"family_members"=>"Sarah"),
+    //   );
+    //    dd($form);
 
       function cleanData(&$str)
       {
@@ -39,8 +56,8 @@ public function export()
       $filename = "website_data_" . date('Ymd') . ".csv";
 
       header("Content-Disposition: attachment; filename=\"$filename\"");
-      header("Content-Type: text/csv");
-
+      header("Content-Encoding: UTF-8 ");
+      header("Content-type: text/csv; charset=UTF-8");
       $out = fopen("php://output", 'w');
 
       $flag = false;
