4147eb4664c924c4e8bcb650b87e3a2e1e496cdb
Montazer Malik <64214350+ste7een@users.noreply.github.com>
2022-06-18 20:29:31 +0300

    export

--- app/Http/Controllers/ExportData.php
+++ app/Http/Controllers/ExportData.php
@@ -9,71 +9,90 @@ use App\Models\Form;
 class ExportData extends Controller
 {
 
-public function export()
-{
-    $data = DB::statement("SET names 'utf8'");
-    $data=[];
-     $forms =Form::all();
+    public function export(){
+        $data = DB::statement("SET names 'utf8'");
+        $data=[];
+        $forms = Form::all();
+
         foreach ($forms as $form) {
+            $family_members ="";
+            foreach ($form->family_members as $member) {
+                $family_members .= "اسم الفرد : $member->name
+                    التولد : $member->birthday
+                    الصلة : $member->kinship
+                    النسب : $member->is_mr_name
+                    العمل : $member->job
+                    الحالة الصحية : $member->health_state
+                    ***********
+                ";
+            }
+            // dd($family_members);
             $data[] = [
-                'id' => $form->id,
-                'name' => $form->family_members()->first()->name,
-                'email' => $form->email,
-                'phone' => $form->phone,
-                'address' => $form->address,
-                'city' => $form->city,
-                'state' => $form->state,
-                'zip' => $form->zip,
-                'country' => $form->country,
-                'level' => $form->level,
-            ];}
+                'ت' => $form->id,
+                'اسم المخول' => $form->user->name,
 
-    //  $S=DB::table('family_members')->get()->toArray();
-    //   $f=array_merge($posts);
+                //Head of the family
+                'رب الاسرة' => $form->head_family->name,
+                'النسب' => $form->head_family->is_mr_name,
+                'العمل' => $form->head_family->job,
+                'الحالة' => $form->head_family->is_alive_name,
+                'الدخل الشهري' => $form->head_family->salary,
+                'رقم هاتف الأب' => $form->person->father_phonenumber,
 
-    // $data = array(
-    //     array("firstname" => "Mary", "lastname" => "Johnson", "age" => 25 ,"family_members"=>"John"),
-    //     array("firstname" => "Amanda", "lastname" => "Miller", "age" => 18,"family_members"=>"Mary"),
-    //     array("firstname" => "James", "lastname" => "Brown", "age" => 31,"family_members"=>"Amanda"),
-    //     array("firstname" => "Patricia", "lastname" => "Williams", "age" => 7,"family_members"=>"James"),
-    //     array("firstname" => "Michael", "lastname" => "Davis", "age" => 43,"family_members"=>"Patricia"),
-    //     array("firstname" => "Sarah", "lastname" => "Miller", "age" => 24,"family_members"=>"Michael"),
-    //     array("firstname" => "Patrick", "lastname" => "Miller", "age" => 27,"family_members"=>"Sarah"),
-    //   );
-    //    dd($form);
+                //Personal Info
+                'مستوى الفقر' => $form->person->level_name,
+                'عنوان السكن' => $form->person->location,
+                'أقرب نقطة دالة' => $form->person->point,
+                'نوع السكن' => $form->person->location_name,
+                'الإيجار' => $form->person->rent,
 
-      function cleanData(&$str)
-      {
+                //Wife
+                'اسم الزوجة' => $form->wife->name,
+                'النسب' => $form->wife->is_mis_name,
+                'الحالة' => $form->wife->state,
+                'رقم هاتف الام' => $form->person->mother_phonenumber,
+
+                //Give
+                'الهبات' => $form->gives->count(),
+
+                //Family Member
+                'عدد افراد الأسرة' => $form->family_members->count(),
+                'معلومات افراد العائلة' => $family_members,
+            ];
+        }
+        // dd($data);
+        function cleanData(&$str){
         if($str == 't') $str = 'TRUE';
         if($str == 'f') $str = 'FALSE';
         if(preg_match("/^0/", $str) || preg_match("/^\+?\d{8,}$/", $str) || preg_match("/^\d{4}.\d{1,2}.\d{1,2}/", $str)) {
-          $str = "'$str";
+            $str = "'$str";
         }
         if(strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"';
-      }
-
-      // filename for download
-      $filename = "website_data_" . date('Ymd') . ".csv";
-
-      header("Content-Disposition: attachment; filename=\"$filename\"");
-      header("Content-Encoding: UTF-8 ");
-      header("Content-type: text/csv; charset=UTF-8");
-      $out = fopen("php://output", 'w');
-
-      $flag = false;
-      foreach($data as $row) {
-
-        if(!$flag) {
-          // display field/column names as first row
-          fputcsv($out, array_keys($row), ',', '"');
-          $flag = true;
         }
-        array_walk($row, __NAMESPACE__ . '\cleanData');
-        fputcsv($out, array_values($row), ',', '"');
-      }
 
-      fclose($out);
-      exit;
+        // filename for download
+        $filename = "seven_snabl" . date('Ymd') . ".csv";
 
-}}
+        header("Content-Disposition: attachment; filename=\"$filename\"");
+        header("Content-Encoding: UTF-8 ");
+        header("Content-type: text/csv; charset=UTF-8");
+        echo chr(0xEF) . chr(0xBB) . chr(0xBF);
+        $out = fopen("php://output", 'w');
+        fputs($out, chr(0xEF) . chr(0xBB) . chr(0xBF));
 
+        $flag = false;
+        foreach($data as $row) {
+
+            if(!$flag) {
+                // display field/column names as first row
+                fputcsv($out, array_keys($row), ',', '"');
+                $flag = true;
+            }
+            array_walk($row, __NAMESPACE__ . '\cleanData');
+            fputcsv($out, array_values($row), ',', '"');
+
+            fclose($out);
+            exit;
+        }
+    }
+}
